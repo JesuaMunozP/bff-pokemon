167352ef5861fd301da5de894a0bd944b66e60fc
JesuaMunozP <110502620+JesuaMunozP@users.noreply.github.com>
2022-09-06 16:16:21 -0400

    Add Test
    First commit

--- src/bff-pokemon/test/bff-pokemon.controller.spec.ts
+++ src/bff-pokemon/test/bff-pokemon.controller.spec.ts
@@ -1,14 +1,40 @@
+import { HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
 import { BffPokemonController } from '../bff-pokemon.controller';
 import { BffPokemonService } from '../bff-pokemon.service';
 
 describe('BffPokemonController', () => {
   let controller: BffPokemonController;
+  const mockService = {
+    byGet: jest.fn((id) => {
+      return {
+        id: '1',
+        name: 'pokemon',
+      };
+    }),
+    byPost: jest.fn((id) => ({
+      id,
+      name: 'pokemon',
+    })),
+    get: jest.fn(),
+    findOne: jest.fn((id) => ({
+      id,
+      name: 'pokemon',
+    })),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BffPokemonController],
-      providers: [BffPokemonService],
+      providers: [BffPokemonService,
+        {
+          provide: HttpService,
+          useValue: mockService,
+        },
+        {
+          provide: BffPokemonService,
+          useValue: mockService,
+        },],
     }).compile();
 
     controller = module.get<BffPokemonController>(BffPokemonController);
